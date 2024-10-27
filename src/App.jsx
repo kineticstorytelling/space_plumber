c9e664acb370be01abfaee5a63fd41784da7cb75
kineticstorytelling <kineticstorytelling@gmail.com>
2024-10-27 19:40:39 +0900

    removed boiler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import './App.css'
 import { Canvas } from '@react-three/fiber'
 import Experience from './components/Experience'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Canvas>
