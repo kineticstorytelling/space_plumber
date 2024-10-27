5d25823691bba203008c039e52e7dd8afc8b3f4f
kineticstorytelling <kineticstorytelling@gmail.com>
2024-10-27 19:37:18 +0900

    experience imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
+import Experience from './components/Experience'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Canvas>
-      
+      <Experience />      
     </Canvas>
   )
 }
