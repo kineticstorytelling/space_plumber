89790b4e26028bab4aa8fed331620882eeb3ee35
kineticstorytelling <kineticstorytelling@gmail.com>
2024-11-21 20:43:01 +0900

    added ambient light

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Canvas } from '@react-three/fiber'
 import Experience from './components/Experience'
+import Spacepl2 from './components/Spacepl2'
 import Spacepl11 from './components/Spacepl11'
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
 
   return (
     <>
     <Canvas>
+      <OrbitControls />
       {/* <Experience />       */}
-      <Spacepl11 />
+      <ambientLight />
+      <directionalLight position={[-5, 5, 5]} castShadow shadow-mapSize={1024} />
+      <Spacepl2 />
+      {/* <Spacepl11 /> */}
     </Canvas>
     </>
   )
