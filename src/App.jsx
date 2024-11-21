bd9ba6037d755a3a40927a89eafdbc7d4db1c884
kineticstorytelling <kineticstorytelling@gmail.com>
2024-11-21 20:56:23 +0900

    camera added to canvas

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-    <Canvas>
-      <OrbitControls />
+    <Canvas camera={{ position: [1, 1.5, 2.5], fov: 50 }}>
+      {/* <OrbitControls /> */}
       <Experience />      
     </Canvas>
     </>
