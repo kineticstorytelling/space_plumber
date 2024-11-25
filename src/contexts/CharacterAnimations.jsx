544845b783d81f92bbb29281af0e0f49ca5ee84f
kineticstorytelling <kineticstorytelling@gmail.com>
2024-11-25 18:54:40 +0900

    usestate added to variable

--- src/contexts/CharacterAnimations.jsx
+++ src/contexts/CharacterAnimations.jsx
@@ -4,7 +4,7 @@ const CharacterAnimationsContext = createContext({});
 
 export const CharacterAnimationsProvider = (props) => {
     const [animationIndex, setAnimationIndex] = useState(0);
-    const [animations, setAnimations]
+    const [animations, setAnimations] = useState([]);
 
     return(
         <CharacterAnimationsContext.Provider value={{}}>
